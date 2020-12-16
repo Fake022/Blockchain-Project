02ae29385c3fdd9c942e51fed28f34c05a2079a7
MARZIN Corentin <corentin.marzin@epitech.eu>
2020-12-16 16:00:25 +0100

    Linked front miner to back correctly. DB call is still needed to fetch transactions from node

--- blockchain-project/controllers/miner_controller.js
+++ blockchain-project/controllers/miner_controller.js
@@ -13,24 +13,29 @@ exports.miner_page = async (req, res) => {
     // var transactions =  await models.Transactions.find({
     //     where: {user_id: user.id}
     // });
-    var transactions = [];
+    var transactions = [{
+        TxNr: 1,
+        amount: 100,
+        fee: 10,
+        from: "fa7942e2ac45ae0294e2a414c64f7f53",
+        to: "766099e79c9a01b047f9ba6f789ab8b3",
+        signature: "d1ea59bf8f03b16c60fe26efeb7a359e"
+    }];
     res.render('miner', {user_email: email, transactions: transactions, hash: req.query.hash, nonce: req.query.nonce});
 };
 
 exports.mine = (req, res) => {
-    console.log(req.body);
+    console.log("body: ", req.body);
     var nonce = 0;
-    var block = req.body.block + req.body.prev + JSON.stringify(req.session.transactions);
-    console.log(block);
+    var block = JSON.stringify(req.body);
     for (let i = 0; ; i++) {
         let toHash = block + i;
         var hash = crypto.createHash("sha256").update(toHash).digest("hex");
-        console.log(hash);
         if (hash.startsWith("00")) {
             nonce = i;
             break;
         }
     }
     console.log("final hash:" +  hash + "\nnonce: " + nonce);
-    res.end(JSON.stringify({code: 200, hash: hash, nonce: nonce}));
+    res.status(200).send({hash: hash, nonce: nonce});
 };
